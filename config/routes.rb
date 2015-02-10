7ecd61a31616f2d256fcf60075a34b08c40335d3
Josiah Ploeger <josiahploeger@gmail.com>
2015-02-10 16:59:49 -0500

    Configured Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
   root 'pages#home'
   get 'about' => 'pages#about' # creates about_path
   get 'mypics' => 'pages#mypics' # creates mypics_path
