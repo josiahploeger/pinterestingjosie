9c3afa82d40a0cbf966fe3afbee0e0c37410459c
Josiah Ploeger <josiahploeger@gmail.com>
2015-02-27 14:57:26 -0500

    added names to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 		
-		#before_filter :configure_permitted_parameters, if: :devise_controller?
-		#protected
+	before_filter :configure_permitted_parameters, if: :devise_controller?
+		protected
 
-		  #def configure_permitted_parameters
-		   # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password,
+		  def configure_permitted_parameters
+		    devise_parameter_sanitizer.for(:sign_up) << :name #{ |u| u.permit(:username, :email, :password,
 		    #  :password_confirmation, :remember_me, :pins, :pins_cache) }
-		    #devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password,
+		    devise_parameter_sanitizer.for(:account_update) << :name #{ |u| u.permit(:username, :email, :password,
 		    #  :password_confirmation, :current_password, :pins, :pins_cache) }
-		#  end
+		  #end
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212193353) do
+ActiveRecord::Schema.define(version: 20150227193121) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150212193353) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "pins"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
